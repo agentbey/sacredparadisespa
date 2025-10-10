6d3d53f03053c688945d46a49a5015f48cc28233
agentbey <creadire@gmail.com>
2025-10-10 15:02:28 +0700

    update

--- js/deznx.js
+++
@@ -1,8 +0,0 @@
-/*! HTML JS Functions v1.0 2016-09 | (c) deznx foundation - David Bey | agentbey.com */
-/* Script for Mobile Menu */
-$(document).ready(function() {
-	$("#toggle").click(function() {
-		$("#navbar").toggle();
-		$('#toggle').toggleClass('on');
-	});
-});
